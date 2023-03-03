7540cb88141e9cdc8c829b9204c1e9e4f827ef68
De <deehzi@pm.me>
2023-03-02 22:55:45 -0300

    feat: adding service to module, generated by nest to events

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { EventsService } from './events/events.service';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EventsService],
 })
 export class AppModule {}
