f78b41f7cfe7b90452cb1e2349dbd9f867110f82
De <deehzi@pm.me>
2023-03-03 15:20:51 -0300

    feat: now all user controller are using the serialize decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('users')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
@@ -31,10 +32,8 @@ export class UsersController {
             body.password,
             body.confirmPassword,
         );
-        console.log(body);
     }
 
-    @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
