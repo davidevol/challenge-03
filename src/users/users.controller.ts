920fb4477feb8a98681b066aa1516a45f7693605
De <deehzi@pm.me>
2023-03-03 15:02:56 -0300

    refactor: now our serialize interceptor accepts any dto arguments

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 
 @Controller('users')
@@ -35,7 +36,7 @@ export class UsersController {
         console.log(body);
     }
 
-    @UseInterceptors(SerializeInterceptor)
+    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
