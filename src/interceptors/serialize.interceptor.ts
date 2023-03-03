754b1391569ce2cd4948561101ddcfff764206f2
De <deehzi@pm.me>
2023-03-03 15:19:04 -0300

    style: using prettierc on file

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,10 +1,15 @@
-import { NestInterceptor, ExecutionContext, CallHandler, UseInterceptors } from '@nestjs/common';
+import {
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+    UseInterceptors,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 
 export function Serialize(dto: any) {
-    return UseInterceptors(new SerializeInterceptor(dto))
+    return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
