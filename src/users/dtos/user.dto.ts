d2a759eb6f41f46553316bfc1c2c8a9aa7c74b98
De <deehzi@pm.me>
2023-03-03 23:03:47 -0300

    feat: adding more details about user for query

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -3,7 +3,22 @@ import { Expose} from 'class-transformer';
 export class UserDto {
     @Expose()
     id: number;
-    
+
+    @Expose()
+    firstName: string;
+
+    @Expose()
+    lastName: string;
+
+    @Expose()
+    birthDate: string;
+
+    @Expose()
+    city: string;
+
+    @Expose()
+    country: string;
+
     @Expose()
     email: string;
 }
