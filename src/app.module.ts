c7fc4ffce49919ca518cc293d8b357f01e4ede51
De <deehzi@pm.me>
2023-03-09 09:49:53 -0300

    feat!: applying a globally scoped pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
 import { UserEntity } from './users/user.entity';
 import { EventEntity } from './events/event.entity';
+
+
+
+const cookieSession = require('cookie-session');
 
 @Module({
     imports: [
@@ -16,5 +23,25 @@ import { EventEntity } from './events/event.entity';
         UsersModule,
         EventsModule,
     ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        {
+            provide: APP_PIPE,
+            useValue: new ValidationPipe({
+                whitelist: true,
+            }),
+        },
+    ],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(
+                cookieSession({
+                    keys: ['some-key'],
+                }),
+            )
+            .forRoutes('*');
+    }
+}
