e2174e4dc5acfffdb724ac93b0e3bcffa959f957
De <deehzi@pm.me>
2023-03-09 11:10:19 -0300

    feat: adding environment path for .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,17 +9,24 @@ import { EventsModule } from './events/events.module';
 import { UserEntity } from './users/user.entity';
 import { EventEntity } from './events/event.entity';
 
-
-
 const cookieSession = require('cookie-session');
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'sqlite',
-            database: 'db.sqlite',
-            entities: [UserEntity, EventEntity],
-            synchronize: true,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                return {
+                    type: 'sqlite',
+                    database: config.get<string>('DB_NAME'),
+                    synchronize: true,
+                    entities: [UserEntity, EventEntity],
+                };
+            },
         }),
         UsersModule,
         EventsModule,
